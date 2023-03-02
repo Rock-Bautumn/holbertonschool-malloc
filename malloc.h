a36c485a79d912614c03211cc8c417c872d2b8b0
Rock-Bautumn <3623@holbertonschool.com>
2023-03-02 09:59:37 -0600

    Betty fix for malloc.h

--- malloc.h
+++ malloc.h
@@ -13,17 +13,28 @@ void *_malloc(size_t size);
 void *add_alloc(void *heapstart, size_t size_to_alloc);
 int _free(void *addr);
 
+/**
+ * struct block_header - Header for malloc allocations
+ * @inuse: true or false depending if allocation is not freed
+ * @allocsize: The size of this allocation
+*/
 
 typedef struct block_header
 {
-    size_t inuse;
-    size_t allocsize;
+	size_t inuse;
+	size_t allocsize;
 } block_t;
+
+/**
+ * struct sblock_header - Small shader for malloc allocations
+ * @inuse: true or false depending if allocation is not freed
+ * @allocsize: The size of this allocation
+*/
 
 typedef struct sblock_header
 {
-    unsigned int inuse;
-    unsigned int allocsize;
+	unsigned int inuse;
+	unsigned int allocsize;
 } sblock_t;
 
 
